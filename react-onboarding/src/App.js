7c475cdb7bbf256b9973a5d58cf8de2c920e39cc
Matias Cassanelli <matias.cassanelli@moove-it.com>
2022-03-31 16:13:49 -0300

    [Task 11] Add login page

--- react-onboarding/src/App.js
+++ react-onboarding/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './views/Home/Home';
+import Login from './views/Login/Login';
 import VideoPlayer from './views/VIdeoPlayer/VideoPlayer';
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Home />} />
+    <Route path="/login" element={<Login />} />
     <Route path="/trailer/:id" element={<VideoPlayer />} />
   </Routes>
 );
