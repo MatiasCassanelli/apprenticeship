7c439f90e1f0c584cb87fa9070c7598709c9cf4b
Matias Cassanelli <matias.cassanelli@moove-it.com>
2022-04-01 13:52:49 -0300

    Add Login page

--- react-onboarding/src/App.js
+++ react-onboarding/src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import useAuth from './hooks/useAuth';
+import getCookie from './utils/getCookie';
 import Home from './views/Home/Home';
 import Login from './views/Login/Login';
 import VideoPlayer from './views/VIdeoPlayer/VideoPlayer';
 
-const App = () => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/trailer/:id" element={<VideoPlayer />} />
-  </Routes>
-);
+const App = () => {
+  const { signIn, isAuthenticated } = useAuth();
+  useEffect(() => {
+    if (!isAuthenticated) {
+      const savedReqToken = getCookie('reqToken');
+      if (savedReqToken) {
+        signIn(savedReqToken);
+      }
+    }
+  }, [isAuthenticated]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login/redirect" element={<Login redirected />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/trailer/:id" element={<VideoPlayer />} />
+    </Routes>
+  );
+};
 
 export default App;
--- react-onboarding/src/views/Login/Login.js
+++ react-onboarding/src/views/Login/Login.js
@@ -1,79 +1,105 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import T from 'prop-types';
 import Footer from '../../components/Footer/Footer';
 import NavBar from '../../components/NavBar/NavBar';
 import styles from './login.module.scss';
+import { getAuthToken } from '../../services/user';
+import useQueryParams from '../../hooks/useQueryParams';
+import useAuth from '../../hooks/useAuth';
 
-const Login = () => (
-  <div className={styles.background}>
-    <NavBar className="relative" isAuthenticated={false} />
-    <div
-      className={`block p-6 rounded-lg shadow-lg bg-white max-w-sm mb-[40px] lg:mb-[100px] mx-[auto] ${styles.loginForm}`}
-    >
-      <h2 className="font-medium leading-tight text-4xl mb-[40px] text-center">
-        Inicia sesión
-      </h2>
-      <form>
-        <div className="form-group mb-5">
-          <input
-            type="email"
-            className="w-full form-control px-3 py-1.5 text-base font-normal bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-            id="exampleInputEmail2"
-            aria-describedby="emailHelp"
-            placeholder="Email o número de teléfono"
-          />
-        </div>
-        <div className="form-group mb-11">
-          <input
-            type="password"
-            className="w-full form-control block px-3 py-1.5 text-base font-normal bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-            id="exampleInputPassword2"
-            placeholder="Contraseña"
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full px-6 py-2.5 mb-6 bg-blue-600 text-white text-base font-normal leading-tight rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out border-none"
-        >
-          Iniciar sesión
-        </button>
-        <div className="flex justify-between items-center mb-7 flex-col md:flex-row">
-          <div className="form-group form-check mb-2 md:mb-0">
+const Login = ({ redirected }) => {
+  const query = useQueryParams();
+  const approved = query.get('approved');
+  const requestToken = query.get('request_token');
+  const { signIn } = useAuth();
+
+  useEffect(() => {
+    if (redirected && approved === 'true') {
+      signIn(requestToken);
+    }
+  }, [redirected]);
+
+  return (
+    <div className={styles.background}>
+      <NavBar className="relative" />
+      <div
+        className={`block p-6 rounded-lg shadow-lg bg-white max-w-sm mb-[40px] lg:mb-[100px] mx-[auto] ${styles.loginForm}`}
+      >
+        <h2 className="font-medium leading-tight text-4xl mb-[40px] text-center">
+          Inicia sesión
+        </h2>
+        <form>
+          <div className="form-group mb-5">
             <input
-              type="checkbox"
-              className="form-check-input text-sm mt-0.5 h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-0 align-top bg-no-repeat bg-center bg-contain float-left cursor-pointer"
-              id="exampleCheck2"
+              type="email"
+              className="w-full form-control px-3 py-1.5 text-base font-normal bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              id="exampleInputEmail2"
+              aria-describedby="emailHelp"
+              placeholder="Email o número de teléfono"
             />
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label
-              className="form-check-label inline-block ml-1 md:ml-6 text-sm"
-              htmlFor="exampleCheck2"
-            >
-              Recuérdame
-            </label>
           </div>
-          <p className="m-0 text-center text-sm">Necesitas ayuda?</p>
-        </div>
-        <div className="text-white text-sm hover:text-gray-300 focus:text-gray-400 transition duration-200 ease-in-out flex justify-center mb-10 mt-10">
-          <img
-            src="/images/facebook.png"
-            alt="facebook icon"
-            className="mr-3"
-          />
-          Iniciar sesión con Facebook
-        </div>
-        <p className="text-center text-sm">
-          ¿Primera vez en Movy?
-          <a
-            href="#!"
-            className="text-white text-sm hover:text-gray-300 focus:text-gray-400 transition duration-200 ease-in-out"
+          <div className="form-group mb-11">
+            <input
+              type="password"
+              className="w-full form-control block px-3 py-1.5 text-base font-normal bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              id="exampleInputPassword2"
+              placeholder="Contraseña"
+            />
+          </div>
+          <button
+            onClick={getAuthToken}
+            type="button"
+            className="w-full px-6 py-2.5 mb-6 bg-blue-600 text-white text-base font-normal leading-tight rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out border-none"
           >
-            Suscríbete ya.
-          </a>
-        </p>
-      </form>
+            Iniciar sesión
+          </button>
+          <div className="flex justify-between items-center mb-7 flex-col md:flex-row">
+            <div className="form-group form-check mb-2 md:mb-0">
+              <input
+                type="checkbox"
+                className="form-check-input text-sm mt-0.5 h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-0 align-top bg-no-repeat bg-center bg-contain float-left cursor-pointer"
+                id="exampleCheck2"
+              />
+              {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+              <label
+                className="form-check-label inline-block ml-1 md:ml-6 text-sm"
+                htmlFor="exampleCheck2"
+              >
+                Recuérdame
+              </label>
+            </div>
+            <p className="m-0 text-center text-sm">Necesitas ayuda?</p>
+          </div>
+          <div className="text-white text-sm hover:text-gray-300 focus:text-gray-400 transition duration-200 ease-in-out flex justify-center mb-10 mt-10">
+            <img
+              src="/images/facebook.png"
+              alt="facebook icon"
+              className="mr-3"
+            />
+            Iniciar sesión con Facebook
+          </div>
+          <p className="text-center text-sm">
+            ¿Primera vez en Movy?
+            <a
+              href="#!"
+              className="text-white text-sm hover:text-gray-300 focus:text-gray-400 transition duration-200 ease-in-out"
+            >
+              Suscríbete ya.
+            </a>
+          </p>
+        </form>
+      </div>
+      <Footer className="pb-0 lg:pb-0" />
     </div>
-    <Footer className="pb-0 lg:pb-0" />
-  </div>
-);
+  );
+};
 
 export default Login;
+
+Login.propTypes = {
+  redirected: T.bool,
+};
+
+Login.defaultProps = {
+  redirected: false,
+};
