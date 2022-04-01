881edb9f0cd71e646aa2ea469db01f1861571fee
Matias Cassanelli <matias.cassanelli@moove-it.com>
2022-04-01 15:36:11 -0300

    Fix broken tests after merging

--- react-onboarding/src/components/UpcomingMovie/UpcomingMovie.test.js
+++ react-onboarding/src/components/UpcomingMovie/UpcomingMovie.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen, waitFor } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 import UpcomingMovie from './UpcomingMovie';
 import * as movieServices from '../../services/movies';
 
@@ -13,54 +15,49 @@ const mockMovie = {
   genres: ['Suspense', 'Action'],
   id: 283552,
 };
+const mockStore = configureStore();
+const store = mockStore({ movies: {} });
 
 test('should render dynamic movie data', () => {
   const date = new Date(mockMovie.release_date);
   const month = new Intl.DateTimeFormat('en', { month: 'long' }).format(date);
   const day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date);
   const releaseDate = `${month} ${day}`;
-  render(<UpcomingMovie movie={mockMovie} />);
+  render(
+    <Provider store={store}>
+      <UpcomingMovie movie={mockMovie} />
+    </Provider>,
+  );
   expect(screen.getByText(mockMovie.title));
   expect(screen.getByText(mockMovie.overview));
   expect(screen.getByText(`Coming ${releaseDate}`));
 });
 
 test('should render video player after fetch video data', async () => {
-  const getVideoSpy = jest.spyOn(movieServices, 'getVideos').mockResolvedValue([
-    {
-      key: '6JnN1DmbqoU',
-      site: 'YouTube',
-      type: 'Trailer',
-    },
-  ]);
-  render(<UpcomingMovie movie={mockMovie} />);
+  const expectedUrl = 'https://www.youtube.com/embed/6JnN1DmbqoU';
+  const getVideoSpy = jest
+    .spyOn(movieServices, 'getVideoUrl')
+    .mockResolvedValue(expectedUrl);
+  render(
+    <Provider store={store}>
+      <UpcomingMovie movie={mockMovie} />
+    </Provider>,
+  );
   await waitFor(() => expect(getVideoSpy).toHaveBeenCalledWith(mockMovie.id));
   const videoPlayer = screen.getByTitle(mockMovie.title);
   expect(videoPlayer).toBeInTheDocument();
-  expect(videoPlayer).toHaveAttribute(
-    'src',
-    'https://www.youtube.com/embed/6JnN1DmbqoU',
-  );
+  expect(videoPlayer).toHaveAttribute('src', expectedUrl);
 });
 
 test('should not render video player because video object is not expected', async () => {
-  const getVideoSpy = jest.spyOn(movieServices, 'getVideos').mockResolvedValue([
-    {
-      key: '6JnN1DmbqoU',
-      site: 'YouTube',
-      type: 'Another type',
-    },
-  ]);
-  render(<UpcomingMovie movie={mockMovie} />);
-  await waitFor(() => expect(getVideoSpy).toHaveBeenCalledWith(mockMovie.id));
-  expect(screen.queryByTitle(mockMovie.title)).not.toBeInTheDocument();
-});
-
-test('should not render video player because there are no videos to show', async () => {
   const getVideoSpy = jest
-    .spyOn(movieServices, 'getVideos')
-    .mockResolvedValue([]);
-  render(<UpcomingMovie movie={mockMovie} />);
+    .spyOn(movieServices, 'getVideoUrl')
+    .mockResolvedValue('');
+  render(
+    <Provider store={store}>
+      <UpcomingMovie movie={mockMovie} />
+    </Provider>,
+  );
   await waitFor(() => expect(getVideoSpy).toHaveBeenCalledWith(mockMovie.id));
   expect(screen.queryByTitle(mockMovie.title)).not.toBeInTheDocument();
 });
