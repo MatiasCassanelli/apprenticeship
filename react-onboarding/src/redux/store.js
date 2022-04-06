a6ab90849871960f5dd9f543f9afc2696b7ae501
Matias Cassanelli <matias.cassanelli@moove-it.com>
2022-04-06 11:47:25 -0300

    Add YouTube player. Save video playback time into redux store

--- react-onboarding/src/redux/store.js
+++ react-onboarding/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import movieReducer from './movies/reducer';
+import playerReducer from './player/reducer';
 
 export default configureStore({
   reducer: {
     movies: movieReducer,
+    player: playerReducer,
   },
 });
